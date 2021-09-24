b7956086704cae65b7b45eeaa857d0bb304f3ebe
KAMILY VITORIA TOSTA <21192995@CFP127ALUNO.LOCAL>
2021-09-24 08:35:30 -0300

    nada não

--- funcoes.php
+++ funcoes.php
@@ -83,3 +83,48 @@ function editarFuncionario($nomeArquivo, $funcionarioEditado){
 
     file_put_contents($nomeArquivo, $json);
 }
+
+
+// 1- usuario vindo do form
+//2- senha vindo do form
+//3- dados do arquivo json de usuario e senha
+
+function realizarLogin($usuario, $senha, $dados){
+
+    foreach ($dados as $dado){
+
+        if ($dado->usuario == $usuario && $dado->senha == $senha) {
+            
+            //VARIAVEIS DE SESSÃO: 
+            $_SESSION["usuario"] = $dado->usuario;
+            $_SESSION["id"] = session_id();
+            $_SESSION["data_hora"] = date('d/m/Y - h:i:s');
+
+            header('location: home.php');
+            exit;
+        }
+        
+    }
+
+    header('location: index.php');
+
+}
+
+// FUNÇÃO DE VALIDAÇÃO DE LOGIN :
+//VERIFICA SE O USUARIO PASSOU PELO PROCESSO DE LOGIN
+
+function verificarLogin(){
+
+    if($_SESSION["id"] != session_id() || (empty($_SESSION["id"])) ){
+
+        header("location: index.php");
+    }
+
+}
+
+function finalizarLogin(){
+    session_unset(); // LIMPA TODAS AS VARIAVEIS D SESSÃO
+    session_destroy(); // DESTROI A SESSÃO ATIVAT
+
+    header('location: index.php');
+}
---
+++ home.php
@@ -0,0 +1,107 @@
+<?php
+
+require("./funcoes.php");
+
+$funcionarios = lerArquivo("./dados/funcionarios.json");
+
+if (isset($_GET["filtro"]) && $_GET["filtro"] != "") {
+  $funcionarios = buscarFuncionario($funcionarios, $_GET["filtro"]);
+}
+
+    session_start();
+    require_once('./funcoes.php');
+
+    verificarLogin();
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="empresa.css" />
+  <script src="./script.js" defer></script>
+  <title>Empresa X</title>
+</head>
+
+<body>
+  <div class="content">
+    <h1>Funcionários da empresa X</h1>
+    <p style="text-align: center">
+      A empresa conta com <em> <?= count($funcionarios) ?> </em> funcionários.
+    </p>
+    <h2>
+
+<?php echo 'Olá ' . strtoupper($_SESSION['usuario']) . ' - Login efetutado em: ' .$_SESSION['data_hora']; ?>
+
+</h2>
+
+<h2>
+
+    <a class="material-icons" href="processa_login.php?logout=true">logout</a>
+        
+</h2>
+    <form method="GET" class="search-form">
+      <div class="input-group" style="flex: 1">
+        <label>Pesquisar por nome</label>
+        <input type="search" placeholder="Digite o nome" name="filtro" value="<?= isset($_GET["filtro"]) ? $_GET["filtro"] : "" ?>" />
+      </div>
+      <button>
+        Buscar
+      </button>
+    </form>
+    <button id="btnAddFuncionario">Adicionar Funcionário</button>
+    <br /><br />
+    <div class="modal-form">
+      <form id="form-funcionario" action="acoes.php" method="POST">
+        <h1>Adicionar funcionário</h1>
+        <input type="text" placeholder="Digite o id" name="id" />
+        <input type="text" placeholder="Digite o primeiro nome" name="first_name" />
+        <input type="text" placeholder="Digite o sobrenome" name="last_name" />
+        <input type="text" placeholder="Digite o e-mail" name="email" />
+        <input type="text" placeholder="Digite o sexo" name="gender" />
+        <input type="text" placeholder="Digite o IP" name="ip_address" />
+        <input type="text" placeholder="Digite o país" name="country" />
+        <input type="text" placeholder="Digite o departamento" name="department" />
+        <button>Salvar</button>
+      </form>
+    </div>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Sobrenome</th>
+        <th>E-mail</th>
+        <th>Sexo</th>
+        <th>Endereço IP</th>
+        <th>País</th>
+        <th>Departamento</th>
+        <th>Editar</th>
+        <th>Excluir</th>
+      </tr>
+      <?php
+      foreach($funcionarios as $funcionario) :
+      ?>
+        <tr>
+          <td><?= $funcionario->id ?></td>
+          <td><?= $funcionario->first_name ?></td>
+          <td><?= $funcionario->last_name ?></td>
+          <td><?= $funcionario->email ?></td>
+          <td><?= $funcionario->gender ?></td>
+          <td><?= $funcionario->ip_address ?></td>
+          <td><?= $funcionario->country ?> </td>
+          <td><?= $funcionario->department ?></td>
+          
+          <td> <button onclick="editar(<?= $funcionario->id ?>)" class="editar">&#9999;</button> </td>
+          <td> <button onclick="deletar(<?= $funcionario->id ?>)" class="excluir">&#128465;</button> </td>
+        </tr>
+      <?php
+      endforeach;
+      ?>
+    </table>
+  </div>
+</body>
+
+</html>
